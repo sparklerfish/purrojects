c3299e7b7225df00ffcfd95459406a28bbb06da3
Julie Adams <julie.ellen.adams@gmail.com>
2020-01-09 13:54:57 -0800

    Style landing page text

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 const Splash = () => (
-    <div className="main-image">
-
-    </div>
-)
+  <div>
+      <div className="splash-container">
+        <div className="main-image">
+        </div>
+        <div className="splash-text">
+            <div className="big-splash-text">MAKE STUFF FOR YOUR CAT</div>
+            <div className="small-splash-text">
+            Purrojects is a community for people who like
+            <br/>
+            to make things for cats. Come explore, share, 
+            <br/>
+            and make your next project for your cat!
+            </div>
+        </div>
+      </div>
+  </div>
+);
 
 export default Splash;
