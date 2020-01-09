132eaed74725bf42aa5071da7565482f2b15f19e
Julie Adams <julie.ellen.adams@gmail.com>
2020-01-08 16:28:03 -0800

    Merge changes

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -39,17 +39,55 @@ class Signup extends React.Component {
     );
   }
 
-          <label>
-            Password:
-            <input
-              type="password"
-              value={this.state.password}
-              onChange={this.handleInput("password")}
-            />
-          </label>
-          {this.renderErrors()}
-          <button onClick={this.handleSubmit}>Sign Up</button>
-        </form>
+  render() {
+    return (
+      <div className="session-image">
+        <div className="dimmed">
+          <div className="sessionbox">
+            <center>
+              <form>
+                <label>
+                  <input
+                    className="session-input"
+                    type="text"
+                    placeholder=" Username"
+                    value={this.state.username}
+                    onChange={this.handleInput("username")}
+                  />
+                </label>
+                <br />
+                <label>
+                  <input
+                    className="session-input"
+                    type="text"
+                    placeholder=" Email"
+                    value={this.state.email}
+                    onChange={this.handleInput("email")}
+                  />
+                </label>
+                <br />
+                <label>
+                  <input
+                    className="session-input"
+                    type="password"
+                    placeholder=" Password"
+                    value={this.state.password}
+                    onChange={this.handleInput("password")}
+                  />
+                </label>
+                <br />
+                <br />
+                {this.renderErrors()}
+                <button className="session-button" onClick={this.handleSubmit}>
+                  Sign Me Up !
+                </button>
+                <p>
+                  Already a member? <Link to="/login">Login >></Link>
+                </p>
+              </form>
+            </center>
+          </div>
+        </div>
       </div>
     );
   }
