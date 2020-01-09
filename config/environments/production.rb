f4cd7ab96787e403a819b4559b5d71885b4e426f
Julie Adams <julie.ellen.adams@gmail.com>
2020-01-08 16:50:04 -0800

    Remove background images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,10 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.serve_static_assets = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
