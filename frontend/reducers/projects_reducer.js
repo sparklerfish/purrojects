598a19aa53373bd60d2eef00dd4e830f5f1f1b87
Julie Adams <julie.ellen.adams@gmail.com>
2020-01-10 11:33:48 -0800

    Create projects reducer

---
+++ frontend/reducers/projects_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_ALL_PROJECTS, RECEIVE_PROJECT, REMOVE_PROJECT } from "../actions/project_actions";
+
+const ProjectsReducer = (oldState = {}, action) => {
+    Object.freeze(oldState);
+    let nextState = Object.assign({}, oldState);
+
+    switch(action.type) {
+        case RECEIVE_ALL_PROJECTS:
+            return action.projects;
+        case RECEIVE_PROJECT:
+            nextState[action.project.id] = action.project;
+            return nextState;
+        case REMOVE_POST:
+            delete nextState[action.projectId]
+            return nextState;
+        default:
+            return oldState;
+    }
+};
+
+export default ProjectsReducer;
