f490bee6ac2ab479712d71ca6f0d1f7d3037df1e
Julie Adams <julie.ellen.adams@gmail.com>
2020-01-17 01:13:54 -0800

    Bootstrap current user

--- frontend/purrojects.jsx
+++ frontend/purrojects.jsx
@@ -7,43 +7,43 @@ import throttle from 'lodash/throttle';
 
 import { closeModal } from './actions/modal_actions'; 
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   let store;
-//   if (window.currentUser) {
-//     const preloadedState = {
-//       session: { id: window.currentUser.id },
-//       entities: {
-//         users: { [window.currentUser.id]: window.currentUser }
-//       }
-//     };
-//     store = configureStore(preloadedState);
-//     delete window.currentUser;
-//   } else {
-//     store = configureStore();
-//   }
-//   // window.store = store; 
-//   // window.getState = store.getState;
-//   // window.dispatch = store.dispatch;
-
-//   // window.closeModal = closeModal; 
-
-//   const root = document.getElementById("root");
-//   ReactDOM.render(<Root store={store} />, root);
-// });
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { id: window.currentUser.id },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  // window.store = store; 
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
 
-  let preloadedState = loadState();
-  let store = configureStore(preloadedState);
-  store.subscribe(throttle(() => {
-    saveState({
-      entities: store.getState().entities,
-      session: store.getState().session
-    })
-  }, 1000));
+  // window.closeModal = closeModal; 
 
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
 });
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+
+//   let preloadedState = loadState();
+//   let store = configureStore(preloadedState);
+//   store.subscribe(throttle(() => {
+//     saveState({
+//       entities: store.getState().entities,
+//       session: store.getState().session
+//     })
+//   }, 1000));
+
+//   const root = document.getElementById("root");
+//   ReactDOM.render(<Root store={store} />, root);
+// });
