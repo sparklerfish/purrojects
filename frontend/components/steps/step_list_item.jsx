db92ab2d210c5c32d7cf990c107324db032b61d8
Julie Adams <julie.ellen.adams@gmail.com>
2020-03-06 16:11:05 -0800

    Handle missing step photos

--- frontend/components/steps/step_list_item.jsx
+++ frontend/components/steps/step_list_item.jsx
@@ -8,7 +8,7 @@ class StepListItem extends React.Component {
         return (
           <div className="project-step">
             <div className="project-step-title">Step {stepNo}: {this.props.step.title}</div>
-            <img className="project-show-image" src={this.props.step.imageUrl} />
+            {this.props.step.imageUrl ? <img className="project-show-image" src={this.props.step.imageUrl} /> : null}
             <div className="project-step-body">{this.props.step.body}</div>
           </div>
         );
