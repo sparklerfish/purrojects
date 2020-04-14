3982f3d99e4f5122667d3e02021372ede73a0205
Julie Adams <julie.ellen.adams@gmail.com>
2020-04-13 22:14:59 -0700

    Change step list item to functional component

--- frontend/components/steps/step_list_item.jsx
+++ frontend/components/steps/step_list_item.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 
-class StepListItem extends React.Component {
-  render() {
-    if (!this.props.step) return null;
-    
-    const stepNo = this.props.idx + 1
-        return (
-          <div className="project-step">
-            <div className="project-step-title">Step {stepNo}: {this.props.step.title}</div>
-            {this.props.step.imageUrl ? <img className="project-show-image" src={this.props.step.imageUrl} /> : null}
-            <div className="project-step-body">{this.props.step.body}</div>
-          </div>
-        );
-
-    }
+const StepListItem = props => {
+  if (!props.step) return null;
+  
+  const stepNo = props.idx + 1
+  
+  return (
+    <div className="project-step">
+      <div className="project-step-title">Step {stepNo}: {props.step.title}</div>
+      {props.step.imageUrl ? <img className="project-show-image" src={props.step.imageUrl} /> : null}
+      <div className="project-step-body">{props.step.body}</div>
+    </div>
+  );
 }
 
 export default StepListItem
