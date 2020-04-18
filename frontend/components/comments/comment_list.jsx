45cac83adc72ed236b9e292c1ad2556892873f5a
Julie Adams <julie.ellen.adams@gmail.com>
2020-04-17 18:04:17 -0700

    Refactor CommentList into functional component

--- frontend/components/comments/comment_list.jsx
+++ frontend/components/comments/comment_list.jsx
@@ -2,39 +2,31 @@ import React from 'react';
 import CommentListItem from './comment_list_item';
 import AddComment from './add_comment';
 
-class CommentList extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            rerender: false
-        }
-    }
-
-    render() {
-        if (!this.props.comments) return null;
-        const comments = Object.values(this.props.comments);
-        return (
-          <div className="comments-container">
-            <div className="project-step-title">{comments.length} Comment{comments.length === 1 ? null : "s"}</div>
-            <br />
-            <ul className="comment-list">
-              {comments.map(comment => (
-                <CommentListItem 
-                  comment={comment}
-                  key={comment.id}
-                  userId={this.props.userId}
-                  currentUser={this.props.currentUser}
-                  destroyComment={this.props.destroyComment}
-                />
-              ))}
-            </ul>
-            <AddComment
-              userId={this.props.userId}
-              projectId={this.props.projectId}
-            />
-          </div>
-        );
-    }
+const CommentList = props => {
+  if (!props.comments) return null;
+  const comments = Object.values(props.comments);
+  
+  return (
+    <div className="comments-container">
+      <div className="project-step-title">{comments.length} Comment{comments.length === 1 ? null : "s"}</div>
+      <br />
+      <ul className="comment-list">
+        {comments.map(comment => (
+          <CommentListItem 
+            comment={comment}
+            key={comment.id}
+            userId={props.userId}
+            currentUser={props.currentUser}
+            destroyComment={props.destroyComment}
+          />
+        ))}
+      </ul>
+      <AddComment
+        userId={props.userId}
+        projectId={props.projectId}
+      />
+    </div>
+  );
 };
 
 export default CommentList;
