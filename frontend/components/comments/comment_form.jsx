ab16ce37440fb7b1e50485c9d14267f8c89049ef
Julie Adams <julie.ellen.adams@gmail.com>
2020-05-18 16:33:34 -0700

    Fix indentation

--- frontend/components/comments/comment_form.jsx
+++ frontend/components/comments/comment_form.jsx
@@ -1,84 +1,84 @@
 import React from 'react';
 
 class CommentForm extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = this.props.comment;
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.cancel = this.cancel.bind(this);
-        this.toggleForm = this.props.toggleForm.bind(this);
+    this.state = this.props.comment;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.cancel = this.cancel.bind(this);
+    this.toggleForm = this.props.toggleForm.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.clearErrors();
+  }
+
+  handleSubmit() {
+    this.props.clearErrors();
+    let comment = Object.assign({}, this.state)
+    this.props.createComment(comment)
+      .then(() => {
+        if (this.props.errors.length === 0) this.toggleForm();
+      })
+  }
+
+  cancel() {
+      this.props.clearErrors();
+      this.toggleForm();
+  }
+
+  update(field) {
+    return e => this.setState({ [field]: e.currentTarget.value });
+  }
+
+  renderErrors() {
+    if (this.props.errors.length > 0) {
+      return (
+          <ul className="comment-errors">
+              {this.props.errors.map((error, i) => (
+                  <li key={`error-${i}`}>{error}</li>
+              ))}
+          </ul>
+      );
+    } else {
+      return null;
     }
+  }
 
-    componentDidMount() {
-        this.props.clearErrors();
-    }
-
-    handleSubmit() {
-        this.props.clearErrors();
-        let comment = Object.assign({}, this.state)
-        this.props.createComment(comment)
-          .then(() => {
-            if (this.props.errors.length === 0) this.toggleForm();
-          })
-    }
-
-    cancel() {
-        this.props.clearErrors();
-        this.toggleForm();
-    }
-
-    update(field) {
-        return e => this.setState({ [field]: e.currentTarget.value });
-    }
-
-    renderErrors() {
-        if (this.props.errors.length > 0) {
-            return (
-                <ul className="comment-errors">
-                    {this.props.errors.map((error, i) => (
-                        <li key={`error-${i}`}>{error}</li>
-                    ))}
-                </ul>
-            );
-        } else {
-            return null;
-        }
-    }
-
-    render() {
-        return (
-          <div className="comment-box">
-            <div className="comment-input">
-              <img src={window.user} className="comment-avatar" />
-              <textarea
-                value={this.state.body}
-                onChange={this.update("body")}
-              />
-            </div>
-            <div className="comment-errors">{this.renderErrors()}</div>
-            <div className="comment-actions">
-              <p className="policy">
-                <br />
-                We have a <b>be nice</b> policy.
-                <br />
-                Please be positive and constructive.
-              </p>
-              <div className="buttons">
-                <span className="cancel-button" onClick={this.cancel}>
-                  Cancel
-                </span>
-                <span
-                  className="comment-submit-button"
-                  onClick={this.handleSubmit}
-                >
-                  Post
-                </span>
-              </div>
-            </div>
+  render() {
+    return (
+      <div className="comment-box">
+        <div className="comment-input">
+          <img src={window.user} className="comment-avatar" />
+          <textarea
+            value={this.state.body}
+            onChange={this.update("body")}
+          />
+        </div>
+        <div className="comment-errors">{this.renderErrors()}</div>
+        <div className="comment-actions">
+          <p className="policy">
+            <br />
+            We have a <b>be nice</b> policy.
+            <br />
+            Please be positive and constructive.
+          </p>
+          <div className="buttons">
+            <span className="cancel-button" onClick={this.cancel}>
+              Cancel
+            </span>
+            <span
+              className="comment-submit-button"
+              onClick={this.handleSubmit}
+            >
+              Post
+            </span>
           </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CommentForm;
