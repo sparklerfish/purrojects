9253c3b05e70a1a3ff7e6be4dcf71416cb7fd408
Julie Adams <julie.ellen.adams@gmail.com>
2020-06-03 19:39:54 -0700

    Refactor signup form with hooks

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -1,43 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom"
 import Footer from "../footer/footer";
 
-class Signup extends React.Component {
-  componentDidMount() {
-    this.props.clearErrors();
+const Signup = props => {
+  useEffect(() => {
+    props.clearErrors();
+    window.scrollTo(0, 0);
+  }, [])
+
+  const [creds, setCreds] = useState({
+    username: "",
+    password: "",
+    email: ""
+  })
+
+  const handleInput = (e) => {
+    setCreds({ ...creds, [e.target.name]: e.target.value });
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      email: "",
-      password: ""
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.renderErrors = this.renderErrors.bind(this);
-  }
-
-  handleInput(type) {
-    return e => {
-      this.setState({ [type]: e.target.value });
-    };
-  }
-
-  handleSubmit(e) {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.createNewUser(this.state);
-    if (!this.props.errors) {
-      (setTimeout(() => this.props.history.goBack(), 100))
+    props.createNewUser(creds);
+    if (!props.errors) {
+      (setTimeout(() => props.history.goBack(), 100))
     }
   }
 
-  renderErrors() {
-    if (this.props.errors.length > 0) {
+  const renderErrors = () => {
+    if (props.errors.length > 0) {
       return (
         <ul className="session-errors">
-          {this.props.errors.map((error, i) => (
+          {props.errors.map((error, i) => (
             <li key={`error-${i}`}>{error}</li>
           ))}
         </ul>
@@ -47,62 +40,63 @@ class Signup extends React.Component {
     }
   }
 
-  render() {
-    return (
-      <div>
-        <div className="session-image">
-          <div className="dimmed">
-            <div className="sessionbox">
-              <center>
-                <form>
-                  <label>
-                    <input
-                      className="session-input"
-                      type="text"
-                      placeholder="Username"
-                      value={this.state.username}
-                      onChange={this.handleInput("username")}
-                    />
-                  </label>
+  return (
+    <>
+      <div className="session-image">
+        <div className="dimmed">
+          <div className="sessionbox">
+            <center>
+              <form>
+                <label>
+                  <input
+                    className="session-input"
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    value={creds.username}
+                    onChange={handleInput}
+                  />
+                </label>
+                <br />
+                <label>
+                  <input
+                    className="session-input"
+                    name="email"
+                    type="text"
+                    placeholder="Email"
+                    value={creds.email}
+                    onChange={handleInput}
+                  />
+                </label>
+                <br />
+                <label>
+                  <input
+                    className="session-input"
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    value={creds.password}
+                    onChange={handleInput}
+                  />
+                </label>
+                <br />
+                <br />
+                {renderErrors()}
+                <button className="session-button" onClick={handleSubmit}>
+                  Sign Me Up !
+                </button>
+                <p>
                   <br />
-                  <label>
-                    <input
-                      className="session-input"
-                      type="text"
-                      placeholder="Email"
-                      value={this.state.email}
-                      onChange={this.handleInput("email")}
-                    />
-                  </label>
-                  <br />
-                  <label>
-                    <input
-                      className="session-input"
-                      type="password"
-                      placeholder="Password"
-                      value={this.state.password}
-                      onChange={this.handleInput("password")}
-                    />
-                  </label>
-                  <br />
-                  <br />
-                  {this.renderErrors()}
-                  <button className="session-button" onClick={this.handleSubmit}>
-                    Sign Me Up !
-                  </button>
-                  <p>
-                    <br />
-                    Already a member? <Link to="/login">Login »</Link>
-                  </p>
-                </form>
-              </center>
-            </div>
+                  Already a member? <Link to="/login">Login »</Link>
+                </p>
+              </form>
+            </center>
           </div>
         </div>
-      <Footer/>
       </div>
-    );
-  }
+    <Footer/>
+    </>
+  );
 }
 
 export default Signup;
